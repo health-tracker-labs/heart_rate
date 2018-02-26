aa7a897d7a9ca144072d2219e1b30354a6df4083
sergtm1 <sergey.timofeev.2000@list.ru>
2018-02-26 19:38:06 +0200

    Added script for creating oracle user for testing.

--- sql/heart_rate_schema.sql
+++ sql/heart_rate_schema.sql
@@ -1,15 +1,15 @@
-DROP TABLE HEART_RATE.HEART_RATE;
-DROP TABLE HEART_RATE.PERSON;
-DROP TABLE HEART_RATE.HELP;
-DROP TABLE HEART_RATE.TOPIC;
-DROP TABLE HEART_RATE.USER;
-DROP SEQUENCE HEART_RATE.PERSON_SEQ;
-DROP SEQUENCE HEART_RATE.HEART_RATE_SEQ;
-DROP SEQUENCE HEART_RATE.HELP_SEQ;
-DROP SEQUENCE HEART_RATE.TOPIC_SEQ;
-DROP SEQUENCE HEART_RATE.USER_SEQ;
+DROP TABLE HEART_RATE;
+DROP TABLE PERSON;
+DROP TABLE HELP;
+DROP TABLE TOPIC;
+DROP TABLE USER;
+DROP SEQUENCE PERSON_SEQ;
+DROP SEQUENCE HEART_RATE_SEQ;
+DROP SEQUENCE HELP_SEQ;
+DROP SEQUENCE TOPIC_SEQ;
+DROP SEQUENCE USER_SEQ;
 
-CREATE TABLE HEART_RATE.PERSON(
+CREATE TABLE PERSON(
 ID INT NOT NULL PRIMARY KEY,
 FIRST_NAME VARCHAR2(20) NOT NULL,
 MIDDLE_NAME VARCHAR2(20),
@@ -22,20 +22,20 @@ MOBILE_PHONE VARCHAR2(30),
 EMAIL VARCHAR2(30)
 );
 
-CREATE SEQUENCE HEART_RATE.PERSON_SEQ;
+CREATE SEQUENCE PERSON_SEQ;
 
-create table heart_rate.USER_(
+create table USERS(
 ID INT not null primary key,
 USERNAME varchar2(20) not null,
 PASSWORD varchar2(20) not null,
 ROLE varchar2(20) not null
 );
 
-CREATE SEQUENCE HEART_RATE.USER_SEQ;
+CREATE SEQUENCE USERS_SEQ;
 
-insert into HEART_RATE.USER_(id, username, password, role) values(1,'admin', 'admin', 'ADMIN');
+insert into USERS(id, username, password, role) values(USERS_SEQ.nextval,'admin', 'admin', 'ADMIN');
 
-CREATE TABLE HEART_RATE.HEART_RATE(
+CREATE TABLE HEART_RATE(
 ID INT NOT NULL PRIMARY KEY,
 UPPER_PRESSURE INT NOT NULL,
 LOWER_PRESSURE INT NOT NULL,
@@ -43,37 +43,37 @@ CONCREATE_DATE DATE NOT NULL,
 PERSON_ID INT NOT NULL
 );
 
-ALTER TABLE HEART_RATE.HEART_RATE ADD CONSTRAINT fk_heart_rate_person_id FOREIGN KEY(PERSON_ID) REFERENCES heart_rate.PERSON(ID) ON DELETE CASCADE ENABLE;
-alter table heart_rate.heart_rate add BPM int default 0 not null
+ALTER TABLE HEART_RATE ADD CONSTRAINT fk_heart_rate_person_id FOREIGN KEY(PERSON_ID) REFERENCES PERSON(ID) ON DELETE CASCADE ENABLE;
+alter table heart_rate add BPM int default 0 not null
 
-CREATE SEQUENCE HEART_RATE.HEART_RATE_SEQ;
+CREATE SEQUENCE HEART_RATE_SEQ;
 
-CREATE TABLE HEART_RATE.HELP(
+CREATE TABLE HELP(
 ID INT NOT NULL PRIMARY KEY,
 NAME VARCHAR2(30) NOT NULL,
 DESCRIPTION CLOB NOT NULL,
 TOPIC_ID INT NOT NULL
 );
 
-CREATE SEQUENCE HEART_RATE.HELP_SEQ;
+CREATE SEQUENCE HELP_SEQ;
 
-CREATE TABLE HEART_RATE.TOPIC(
+CREATE TABLE TOPIC(
 ID INT NOT NULL PRIMARY KEY,
 NAME VARCHAR2(30) NOT NULL
 );
 
-CREATE SEQUENCE HEART_RATE.TOPIC_SEQ;
-ALTER TABLE HEART_RATE.HELP ADD CONSTRAINT fk_help_topic_id FOREIGN KEY(TOPIC_ID) REFERENCES heart_rate.TOPIC(ID) ON DELETE CASCADE ENABLE;
+CREATE SEQUENCE TOPIC_SEQ;
+ALTER TABLE HELP ADD CONSTRAINT fk_help_topic_id FOREIGN KEY(TOPIC_ID) REFERENCES TOPIC(ID) ON DELETE CASCADE ENABLE;
 
-insert into heart_rate.TOPIC(id, NAME) values(HEART_RATE.TOPIC_SEQ.nextval, 'PERSON');
-insert into heart_rate.TOPIC(id, NAME) values(HEART_RATE.TOPIC_SEQ.nextval, 'HEART_RATE');
+insert into TOPIC(id, NAME) values(TOPIC_SEQ.nextval, 'PERSON');
+insert into TOPIC(id, NAME) values(TOPIC_SEQ.nextval, 'HEART_RATE');
 
-insert into heart_rate.help (id, NAME, description, TOPIC_ID) values (HEART_RATE.HELP_SEQ.nextval, 'Add Heart Rate', '��� ������� ��������� �������� �������� � ���� ������ '
+insert into help (id, NAME, description, TOPIC_ID) values (HELP_SEQ.nextval, 'Add Heart Rate', '��� ������� ��������� �������� �������� � ���� ������ '
 || '� ���������� ����������� ������ �� �����.'
 || ' ���������: upperPressure - ���� ������� �������t, lowerPressure - ���� ������ ��������, date - ������������ �������� ������� ��������� � ���� ������� �������� ����, 
 �� ��������� ��������� ����� �������� ����������,'
 || 'firstName - ��� ������ person, secondName - ������� ������ person', 2);
 
-insert into heart_rate.help (id, NAME, description, TOPIC_ID) values (HEART_RATE.HELP_SEQ.nextval, 'Add Heart Rate By Id', '��� ������� �������� �������� � ���� ������, � ����� ������������ ����� �� ������ ID ����� ������ � PERSON.'
+insert into help (id, NAME, description, TOPIC_ID) values (HELP_SEQ.nextval, 'Add Heart Rate By Id', '��� ������� �������� �������� � ���� ������, � ����� ������������ ����� �� ������ ID ����� ������ � PERSON.'
 || ' ���������: id - ���������� �������� ������������� ������ PERSON, upperPressure - ���� ������� �������t, lowerPressure - ���� ������ ��������, date - ������������ �������� ������� ��������� � ���� ������� �������� ����, 
 �� ��������� ��������� ����� �������� ����������.', 2);
--- sql/heart_rate_schema_25.02.18.sql
+++ sql/heart_rate_schema_25.02.18.sql
@@ -1,10 +1,10 @@
-create table heart_rate.USERS(
+create table USERS(
 ID INT not null primary key,
 USERNAME varchar2(20) not null,
 PASSWORD varchar2(20) not null,
 ROLE varchar2(20) not null
 );
 
-CREATE SEQUENCE HEART_RATE.USER_SEQ;
+CREATE SEQUENCE USERS_SEQ;
 
-insert into HEART_RATE.USERS(id, username, password, role) values(USER_SEQ.nextval,'admin', 'admin', 'ADMIN');
+insert into USERS(id, username, password, role) values(USERS_SEQ.nextval,'admin', 'admin', 'ADMIN');
