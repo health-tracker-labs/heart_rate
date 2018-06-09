1853fe7b1af602b4300632d6d232b4c846c241b8
Sergey Timofeev <sergey.timofeev.2000@list.ru>
2018-06-09 16:18:54 +0300

    Revert "Implementation of embedded angular module."
    This reverts commit f69fc82e4cef4e278e7ce1ba075b2b0acf81c036.

--- src/main/java/com/sergtm/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/sergtm/configuration/security/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.sergtm.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
