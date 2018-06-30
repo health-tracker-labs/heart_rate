d2473376112ebc7e30adecd2f9c3d6ae0d3581fe
sergtm1 <sergey.timofeev.2000@list.ru>
2018-06-30 12:23:38 +0200

    Added more concrete failure massages during search validation.

--- src/main/resources/static/app/controller/MainToolBarController.js
+++ src/main/resources/static/app/controller/MainToolBarController.js
@@ -8,11 +8,20 @@ Ext.define('app.controller.MainToolBarController', {
 
     onSearchClick: function () {
         var toolBar = this.getView();
-        var from = toolBar.getReferences().from_date.getValue();
-        var to = toolBar.getReferences().to_date.getValue();
+        var fromDateField = toolBar.getReferences().from_date;
+        var toDateField = toolBar.getReferences().to_date;
+        var from = fromDateField.getValue();
+        var to = toDateField.getValue();
         var chart = Ext.ComponentQuery.query("#chart")[0];
         var personId = toolBar.getReferences().personCombobox.getValue();
-        if (toolBar.isValid()) {
+        var redraw = true;
+
+        if (from > to) {
+            Ext.Msg.alert('Failed', 'From date is later than to date');
+            redraw = false;
+        }
+
+        if (toolBar.isValid() && redraw) {
             chart.getStore().load({
                 params: {
                     personId: personId,
@@ -22,7 +31,13 @@ Ext.define('app.controller.MainToolBarController', {
             });
             chart.redraw();
         } else {
-            Ext.Msg.alert('Failed', 'You entered invalid dates');
+            if (!fromDateField.isValid() && !toDateField.isValid()) {
+                Ext.Msg.alert('Failed', 'You entered invalid from and to dates');
+            } else if (!fromDateField.isValid()) {
+                Ext.Msg.alert('Failed', 'You entered invalid from date');
+            } else if (!toDateField.isValid()) {
+                Ext.Msg.alert('Failed', 'You entered invalid to date');
+            }
         }
     }
 });
