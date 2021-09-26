628ca29427874b17dd5f408a58f74abc20502e40
Aleksey Timofeev <alextim@list.ru>
2021-09-26 12:09:19 +0300

    Added swagger to application

--- src/main/java/com/sergtm/configuration/web/WebConfig.java
+++ src/main/java/com/sergtm/configuration/web/WebConfig.java
@@ -1,19 +1,21 @@
 package com.sergtm.configuration.web;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+
         super.addResourceHandlers(registry);
     }
 
@@ -24,6 +26,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass (JstlView.class);
         registry.viewResolver(viewResolver);
-
     }
 }
